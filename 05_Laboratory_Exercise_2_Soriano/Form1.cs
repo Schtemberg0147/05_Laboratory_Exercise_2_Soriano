01068ba53f3aacfcd5244ae3c1f88550f206c057
Schtemberg0147 <joseiiis013@gmail.com>
2025-10-24 16:13:32 +0800

    Add project files.

---
+++ 05_Laboratory_Exercise_2_Soriano/Form1.cs
@@ -0,0 +1,10 @@
+namespace _05_Laboratory_Exercise_2_Soriano
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
