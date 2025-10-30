802848cb6899888de83014ba732f7a12c5975fad
Schtemberg0147 <joseiiis013@gmail.com>
2025-10-30 21:57:06 +0800

    Created new form "FrmStudentRecord"

---
+++ 05_Laboratory_Exercise_2_Soriano/FrmRegistration.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace _05_Laboratory_Exercise_2_Soriano
+{
+    partial class FrmRegistration
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblRegistration = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            dateTimePicker1 = new DateTimePicker();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            comboBox1 = new ComboBox();
+            comboBox2 = new ComboBox();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // lblRegistration
+            // 
+            lblRegistration.AutoSize = true;
+            lblRegistration.Font = new Font("Bahnschrift", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblRegistration.Location = new Point(19, 20);
+            lblRegistration.Name = "lblRegistration";
+            lblRegistration.Size = new Size(144, 29);
+            lblRegistration.TabIndex = 0;
+            lblRegistration.Text = "Registration";
+            lblRegistration.Click += lblRegistration_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Bahnschrift SemiBold", 12F, FontStyle.Bold);
+            label2.Location = new Point(40, 108);
+            label2.Name = "label2";
+            label2.Size = new Size(128, 19);
+            label2.TabIndex = 1;
+            label2.Text = "Student Number";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Bahnschrift SemiBold", 12F, FontStyle.Bold);
+            label3.Location = new Point(81, 160);
+            label3.Name = "label3";
+            label3.Size = new Size(87, 19);
+            label3.TabIndex = 2;
+            label3.Text = "Last Name";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Bahnschrift SemiBold", 12F, FontStyle.Bold);
+            label4.Location = new Point(131, 210);
+            label4.Name = "label4";
+            label4.Size = new Size(37, 19);
+            label4.TabIndex = 3;
+            label4.Text = "Age";
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Font = new Font("Bahnschrift", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            dateTimePicker1.Location = new Point(186, 268);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(213, 26);
+            dateTimePicker1.TabIndex = 4;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Bahnschrift SemiBold", 12F, FontStyle.Bold);
+            label5.Location = new Point(98, 269);
+            label5.Name = "label5";
+            label5.Size = new Size(70, 19);
+            label5.TabIndex = 5;
+            label5.Text = "Birthday";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Bahnschrift SemiBold", 12F, FontStyle.Bold);
+            label6.Location = new Point(480, 273);
+            label6.Name = "label6";
+            label6.Size = new Size(127, 19);
+            label6.TabIndex = 9;
+            label6.Text = "Contact Number";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Bahnschrift SemiBold", 12F, FontStyle.Bold);
+            label7.Location = new Point(545, 209);
+            label7.Name = "label7";
+            label7.Size = new Size(62, 19);
+            label7.TabIndex = 8;
+            label7.Text = "Gender";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Bahnschrift SemiBold", 12F, FontStyle.Bold);
+            label8.Location = new Point(518, 159);
+            label8.Name = "label8";
+            label8.Size = new Size(89, 19);
+            label8.TabIndex = 7;
+            label8.Text = "First Name";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Bahnschrift SemiBold", 12F, FontStyle.Bold);
+            label9.Location = new Point(533, 108);
+            label9.Name = "label9";
+            label9.Size = new Size(74, 19);
+            label9.TabIndex = 6;
+            label9.Text = "Program";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Bahnschrift SemiBold", 12F, FontStyle.Bold);
+            label10.Location = new Point(384, 159);
+            label10.Name = "label10";
+            label10.Size = new Size(33, 19);
+            label10.TabIndex = 10;
+            label10.Text = "M.I.";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(629, 210);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(121, 23);
+            comboBox1.TabIndex = 11;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(629, 108);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(121, 23);
+            comboBox2.TabIndex = 12;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(186, 108);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 13;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(186, 160);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(152, 23);
+            textBox2.TabIndex = 14;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(186, 210);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(59, 23);
+            textBox3.TabIndex = 15;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(629, 269);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(100, 23);
+            textBox4.TabIndex = 18;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(629, 160);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(100, 23);
+            textBox5.TabIndex = 17;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(448, 160);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(46, 23);
+            textBox6.TabIndex = 16;
+            // 
+            // button1
+            // 
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Bahnschrift", 12F);
+            button1.Location = new Point(374, 372);
+            button1.Name = "button1";
+            button1.Size = new Size(110, 34);
+            button1.TabIndex = 19;
+            button1.Text = "Register";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Bahnschrift", 12F);
+            button2.Location = new Point(644, 373);
+            button2.Name = "button2";
+            button2.Size = new Size(110, 34);
+            button2.TabIndex = 20;
+            button2.Text = "Records";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // FrmRegistration
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox4);
+            Controls.Add(textBox5);
+            Controls.Add(textBox6);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(comboBox2);
+            Controls.Add(comboBox1);
+            Controls.Add(label10);
+            Controls.Add(label6);
+            Controls.Add(label7);
+            Controls.Add(label8);
+            Controls.Add(label9);
+            Controls.Add(label5);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(lblRegistration);
+            Name = "FrmRegistration";
+            Text = "FrmRegistration";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblRegistration;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private DateTimePicker dateTimePicker1;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private ComboBox comboBox1;
+        private ComboBox comboBox2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private Button button1;
+        private Button button2;
+    }
+}
